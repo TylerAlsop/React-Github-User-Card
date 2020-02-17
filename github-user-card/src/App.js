3a3241f4ac8fda74e6290b1186d5c2bc4e5306a1
Tyler Alsop <tylerjalsop@gmail.com>
2020-02-16 18:59:15 -0700

    Setup an axios call to get my github user data inside of componentDidMount. Added a componentDidUpdate(). All in App.js

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      user: []
+      userInfo: []
     };
+  }
+
+  componentDidMount() {
+    console.log("Component Mount")
+
+    axios
+      .get("https://api.github.com/users/TylerAlsop")
+      .then(res => {
+        console.log("This is the API Data:", res)
+        this.setState({ userInfo: res })
+      })
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Somponent Update")
+
   }
 
 
@@ -16,6 +34,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <div>Here's To The Start Of A New App</div>
+          
         </header>
       </div>
     );
