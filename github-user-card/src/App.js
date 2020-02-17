3db0890d2faf8e80d6a41eaa5ff9ca9b95067a79
Tyler Alsop <tylerjalsop@gmail.com>
2020-02-16 18:22:32 -0700

    Added the constructor() and super() to App.js

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: []
+    };
+  }
+
+  
 
   render () {
     return (
